8e67532f5fa50e1d8d1c2b6447fa5c825be6b600
H.A. de Groot <hugo.de.groot@hva.nl>
2021-03-01 15:03:25 +0100

    I Am Rich App

---
+++ I am rich/I am rich/I am rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am rich
+//
+//  Created by Hugo de Groot on 09/02/2021.
+//  Copyright © 2021 Hugo de Groot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
